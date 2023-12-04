a269ec72b9d823de7ab6789910e86ba3bdbdde09
Ali Nik <alinikandish001@gmail.com>
2023-12-04 18:17:57 +0330

    feat: Add Priority Component

---
+++ app/(components)/Priority.tsx
@@ -0,0 +1,30 @@
+import { log } from 'console'
+import React from 'react'
+import { FaFire } from 'react-icons/fa'
+
+type PriorityProps={
+    priority:number,
+}
+const Priority = (priority:PriorityProps) => {
+  return (
+    <div className="flex gap-x-px ">
+            <div className={priority.priority > 0 ? `text-red-500` : `text-gray-500`}>
+              <FaFire />
+            </div>
+            <div className={priority.priority > 1 ? `text-red-500` : `text-gray-500`}>
+              <FaFire />
+            </div>
+            <div className={priority.priority > 2 ? `text-red-500` : `text-gray-500`}>
+              <FaFire />
+            </div>
+            <div className={priority.priority > 3 ? `text-red-500` : `text-gray-500`}>
+              <FaFire />
+            </div>
+            <div className={priority.priority > 4 ? `text-red-500` : `text-gray-500`}>
+              <FaFire />
+            </div>
+    </div>
+  )
+}
+
+export default Priority
