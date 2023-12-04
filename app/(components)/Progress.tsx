00b1132a8283eec5edfa7cce47235cf82c46b429
Ali Nik <alinikandish001@gmail.com>
2023-12-04 18:21:11 +0330

    feat: Add Progress Component

---
+++ app/(components)/Progress.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+type ProgressProps={
+    progress:number
+}
+const Progress = (progress:ProgressProps) => {
+  return (
+    <div className="mt-4 mb-5">
+          <p className="text-gray-600">درصد پیشرفت</p>
+          <div className="bg-gray-400 w-64 h-3 rounded-lg mt-2 overflow-hidden">
+          <div style={{width:`${progress}%`}} className={`bg-red-500 h-full rounded-lg shadow-md`}></div>
+          </div>
+        </div>
+  )
+}
+
+export default Progress
