650afebfd6f14a777def4420b63d87d21087f926
Ali Nik <alinikandish001@gmail.com>
2023-12-05 12:43:02 +0330

    Style:Add Vazir Font

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className} dir='rtl'>
+      <body className='font-VazirMedium' dir='rtl'>
         <Navbar/>
         {children}
       </body>
