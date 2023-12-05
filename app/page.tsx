5b21f6501200789b208fe496b305b876313fbfcd
Ali Nik <alinikandish001@gmail.com>
2023-12-05 12:48:41 +0330

    update: Updated Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,40 @@
-import Image from 'next/image'
+import TicketCard from './(components)/TicketCard'
 
-export default function Home() {
+type ticket={
+  _id:string,
+    title:string,
+    description:string,
+    progress:number,
+    priority:number,
+    category:string,
+    createdAt:string
+}
+const getAllTickets=async()=>{
+  try {
+    const res=await fetch("http://localhost:3000/api/Tickets",{
+      cache:"no-store",
+    })
+    if (!res.ok) {
+      throw new Error("Failed to fetch topics");
+    }
+
+    return res.json();
+    
+  } catch (error) {
+    console.log("Error loading topics: ", error);
+  }
+
+}
+
+export default async function Home() {
+  const allTickets = await getAllTickets();
+  const allTicketsData=allTickets.allTickets
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     سلام
-    </main>
+    <div className="flex flex-wrap justify-center gap-5 mt-10">
+      {allTicketsData.map((ticket:ticket)=>{
+        return <TicketCard {...ticket}/>
+      })}
+</div>
+
   )
 }
