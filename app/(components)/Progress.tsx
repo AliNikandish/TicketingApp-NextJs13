b1ed425a76c8fadbcbdd19515b8b9932662e2001
Ali Nik <alinikandish001@gmail.com>
2023-12-05 14:09:13 +0330

    fix: fix ProgressBar

--- app/(components)/Progress.tsx
+++ app/(components)/Progress.tsx
@@ -4,11 +4,12 @@ type ProgressProps={
     progress:number
 }
 const Progress = (progress:ProgressProps) => {
+  console.log(progress)
   return (
     <div className="mt-4 mb-5">
           <p className="text-gray-600">درصد پیشرفت</p>
           <div className="bg-gray-400 w-64 h-3 rounded-lg mt-2 overflow-hidden">
-          <div style={{width:`${progress}%`}} className={`bg-red-500 h-full rounded-lg shadow-md`}></div>
+          <div style={{width:`${progress.progress}%`}} className={`bg-red-500 h-full rounded-lg shadow-md`}></div>
           </div>
         </div>
   )
