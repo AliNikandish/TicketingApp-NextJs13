890d7fa3afd671b97e84116369d5ef2eb1bf32c7
Ali Nik <alinikandish001@gmail.com>
2023-12-07 11:25:40 +0330

    feat:Add Loading Page

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div className="flex gap-4 flex-wrap justify-center items-center min-h-screen">
+    <img className="w-20 h-20 animate-spin" src="https://www.svgrepo.com/show/448500/loading.svg" alt="Loading icon"/>
+</div>
+  )
+}
+
+export default loading
