d3b5d0f96e840a89d626e91ea71ac15ecd539a3d
Ali Nik <alinikandish001@gmail.com>
2023-12-07 11:42:20 +0330

    Fix:Change ticketCard Style for Closing Ticket

--- Types/Index.ts
+++ Types/Index.ts
@@ -3,3 +3,14 @@ type params={
         id:string
     }
 }
+
+type TicketCardProps={
+    _id:string,
+    title:string,
+    description:string,
+    progress:number,
+    priority:number,
+    category:string,
+    createdAt:string,
+    status:string
+}
--- app/(components)/TicketCard.tsx
+++ app/(components)/TicketCard.tsx
@@ -3,15 +3,6 @@ import Link from "next/link";
 import Priority from "./Priority";
 import Progress from "./Progress";
 
-type TicketCardProps={
-    _id:string,
-    title:string,
-    description:string,
-    progress:number,
-    priority:number,
-    category:string,
-    createdAt:string
-}
 const TicketCard = ({
   _id,
   title,
@@ -19,11 +10,12 @@ const TicketCard = ({
   progress,
   priority,
   category,
-  createdAt
+  createdAt,
+  status
 }:TicketCardProps) => {    
   return (
     <div className="p-4 max-w-sm h-[360px] ">
-      <div className="bg-slate-50 p-10 rounded-lg shadow-md">
+      <div className={`${status==="بسته" ? 'bg-slate-300/90' : 'bg-slate-50' } p-10 rounded-lg shadow-md`}>
         <h1 className="text-xl font-bold">عنوان : {title}</h1>
         <div className="flex gap-x-0.5 items-center mt-1">
           <p>اولویت:</p>
@@ -34,6 +26,9 @@ const TicketCard = ({
         </div>
         <Progress progress={progress}/>
         <h2 className="tracking-wide line-clamp-1">{description}</h2>
+        <div className="flex gap-x-1 items-center text-gray-600">
+          <p className="">وضعیت : {status}</p>
+        </div>
         <p className="text-gray-400 text-xs mt-1">تاریخ ایجاد :{createdAt}</p>
         <div className="mt-4">
           <Link
