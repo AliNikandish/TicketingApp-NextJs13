e461f70271257367e4430a779f55777992ab2467
Ali Nik <alinikandish001@gmail.com>
2023-12-08 13:44:48 +0330

    Feat:Add Logic for delete Ticket

--- app/(components)/DeleteButton.tsx
+++ app/(components)/DeleteButton.tsx
@@ -5,8 +5,18 @@ import { useRouter } from "next/navigation";
 const DeleteButton = ({id}:DeleteButton) => {
   const router = useRouter();
  
+  const deleteTicket = async ()=>{
+    const res = await fetch(`http://localhost:3000/api/tickets/${id}`,{
+      method:"DELETE",
+    });
+    if(res.ok){
+      router.push('/')
+      router.refresh()
+    }
+  }
   return (
     <button
+    onClick={deleteTicket}
       type="button"
       className="bg-red-500 text-white py-1 px-4 rounded shadow"
     >
