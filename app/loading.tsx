36f07ec777f055ed38ca90e9ecb8fbc0f2742087
Ali Nik <alinikandish001@gmail.com>
2023-12-08 13:29:47 +0330

    Fix:Update Loading page

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const loading = () => {
   return (
-    <div className="flex gap-4 flex-wrap justify-center items-center min-h-screen">
-    <img className="w-20 h-20 animate-spin" src="https://www.svgrepo.com/show/448500/loading.svg" alt="Loading icon"/>
+    <div className="flex gap-4 flex-wrap justify-center items-center min-h-[500px]">
+    <div className="border-gray-300 h-20 w-20 animate-spin rounded-full border-8 border-t-black" />
 </div>
   )
 }
